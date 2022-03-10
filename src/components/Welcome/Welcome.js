1ae4bde1bd4a80b7920f5497f7ce0b6759df0ef8
Cesar Val Leiva <cesarvalleiva@gmail.com>
2022-03-09 22:14:25 -0300

    modificacion en Welcome

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -4,7 +4,7 @@ import './Welcome.css'
 const Welcome = (props) => {
   return (
     <div className='welcome'>
-        <h2>Bienvenido {props.nombre} {props.apellido}</h2>
+        <h2>Bienvenido {props.nombre} {props.apellido}!!</h2>
     </div>
   )
 }
